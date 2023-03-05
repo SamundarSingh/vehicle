6b9829c01869bda4dd16329efc93c5006ce16d60
samundar singh <samundarsingh5@gmail.com>
2023-03-05 20:17:08 +0530

    intial commit from branch-3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Vehicle in Main");
         System.out.println("Second Line");
+        System.out.println("Line added from branch-3");
     }
 }
